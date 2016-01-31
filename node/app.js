5a0f960d911c95b275504f8ed5ea838091246a62
Taylor Zhang <taylor.zhang@vanderbilt.edu>
2016-01-30 22:23:05 -0600

    unfinished skeleton for backend methods

---
+++ node/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.get('/users', function(req, res) {
+
+}
+
+app.get('/users:id', function (req, res) {
+
+}
+
+app.post('/users', function (req, res) {
+
+}
+
+app.delete('/users:id', function (req, res) {
+
+}
+
+app.listen(3000);
+console.log('Listening on port 3000...');
